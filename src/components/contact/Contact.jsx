2553e799e453889f716f9930fdcf83f5a078b6a8
its-pikesh <pikesh.11606271@lpu.in>
2022-11-15 11:46:47 +0530

    contact component added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+/* eslint-disable react/jsx-no-target-blank */
+import React, { useRef } from "react";
 import "./Contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
+import emailjs from "emailjs-com";
+
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm("Your Service Id", "your template id", form.current, "your userid");
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>pikesh.patel492@gmail.com</h5>
+            <a href="mailto:pikesh.patel492@gmail.com">Send a message</a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>pikesh.patel492@gmail.com</h5>
+            <a href="https://m.me/pikesh1212" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>ping me</h5>
+            <a href="https://api.whatsapp.com/send?phone=918602400701">Send a message</a>
+          </article>
+        </div>
+        {/* End of contact options */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" />
+          <textarea name="message" rows="7" placeholder="Your Message"></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,7 +1,59 @@
 import React from "react";
 import "./Testimonial.css";
+import AVTR1 from "../../assets/portfolio1.jpg";
+import AVTR2 from "../../assets/portfolio1.jpg";
+
+//impoer swiper core and required modules
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+//Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
 const Testimonial = () => {
-  return <section id="testimonial">Testimonial</section>;
+  const data = [
+    {
+      avatar: AVTR1,
+      name: "Adarsh Pawar",
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt optio hic maxime ipsa eius nesciunt quisquam quas, ad aliquam neque.",
+    },
+    {
+      avatar: AVTR2,
+      name: "Arun Chaudhary",
+      review:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt optio hic maxime ipsa eius nesciunt quisquam quas, ad aliquam neque.",
+    },
+  ];
+
+  return (
+    <section id="testimonial">
+      <h5>Review From Customer</h5>
+      <h2>Testimonial</h2>
+
+      <Swiper
+        className="container testimonial__container"
+        navigation
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonial;
